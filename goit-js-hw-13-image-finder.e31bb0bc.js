f92b9f2189ad0543d3821fc784d47db8f043882f
Alexandr Chepak <djalexbc@gmail.com>
2020-11-21 16:07:28 +0200

    Updates

--- goit-js-hw-13-image-finder.e31bb0bc.js
+++ goit-js-hw-13-image-finder.e31bb0bc.js
@@ -2633,7 +2633,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56395" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54913" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
